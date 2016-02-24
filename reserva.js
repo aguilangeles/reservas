5bb17b1d6951067be8259cb0f3d5a6777cb8dcc9
aguilangeles <aguilangeles@gmail.com>
2016-02-23 22:39:43 -0300

    ejorando los closures

--- reserva.js
+++ reserva.js
@@ -6,61 +6,64 @@ El objetivo es que no tomen dos personas el mismo turno.
 
 //crear la grilla de horarios.
 // horario tiene que tener un booleano de reservado o no.
-//
 
+function afunction(){
+	var _this =this;
 
+	return {
+		//retorna un listado de objetos hora
+		horariosDisponibles: function(){
+			var milista = [];
+			for (var i = 9 ; i < 24; i++) {
+				var index = i;
+				var objs = {
+					id: index,
+					isReserved : false,
+					reserveCode:'',
+					name:''
+				}
+				milista.push(objs);
+			}
+			return milista;
+		},
+		setearHorarios:function(id){
+			var lista=this.horariosDisponibles();
+			console.log(lista[id]);
 
+		},
+		cargarHorario : function() {
+			var lista=this.horariosDisponibles();
+			for(var i = 0; i < lista.length; i++){
+				var objs = lista[i];
+				var item = $('<li class="btn btn-default" id="'+objs.id +'" >'+objs.id+':00'+'</li>');
+				$('#horario').append(item);
+			}
+		},
+		cargarPlanilla : function() {
+			var lista=this.horariosDisponibles();
+			for(var i = 0; i < lista.length; i++){
+				var objs = lista[i];
+				var item = $('<li class="list-group-item" id="'+objs.id +'" >'+objs.id+'  '+ objs.isReserved+'</li>');
+				$('#planilla').append(item);
+			}
+		},
 
-var reservaAPP = {
+		click : function(afun){
+			var id;
+			var elemento = $('.btn');
+			elemento.on('click', function(){
 
-	lista : {},
-
-	reserva: function(id){
-
-		var person = prompt("Reservá la sala a las "+id+':00, ingresando tu nombre' ,"Harry Potter");
-
-		var upload = this.cargarObjeto();
-
-		console.log(upload[id].isReserved=true);
-
-	},
-	cargarObjeto: function(){
-		var milista = [];
-		for (var i = 9 ; i < 24; i++) {
-
-			var index = i;
-
-			var objs = {
-				id: index,
-				isReserved : false,
-				reserveCode:'',
-				name:''
-
-			} 
-			milista.push(objs);
+				alert(this.id);
+			});
 		}
-		return milista;
-
-	},
-	cargarHorario : function() {
-		var lista=this.cargarObjeto();
-		console.log(lista);
-
-		for(var i = 0; i< lista.length; i++){
-			var objs = lista[i];
-			console.log(objs.id);
-			var item = $('<li class="list-group-item disabled" id="'+objs.id+'">'+objs.id+':00'+'</li>');
-			var button = $('<button class="btn btn-success pull-right" type="button" onclick="APP.reserva('+objs.id+')">reservala ahora!</button>');
-	    	
-	    	$("#horario").append(item);
-	    	$("#"+objs.id).append(button);
-
-		}
-
-    
-     //console.log(lista)
- }
-
+	}
 };
 
-window.APP = reservaAPP;
+var app = new afunction();
+app.cargarHorario();
+app.cargarPlanilla();
+app.click();
+
+
+
+
